23501c487f682fae968fad6ac97aee66812b7e20
Nikita <nickgruzincev98@mail.ru>
2026-05-21 21:31:09 +0300

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -10,9 +10,9 @@ const isPublicRoute = createRouteMatcher([
   "/api/activities(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, request) => {
+export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
-    await auth.protect();
+    auth().protect();
   }
 });
 
