07dd4beeb2d6a7bd3af42a49f80498ff826ad225
Nikita <nickgruzincev98@mail.ru>
2026-05-23 09:38:40 +0300

    feat: switch to Maptiler

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -31,7 +31,7 @@ const Map = forwardRef<MapHandle, Props>(({ activities = [], onMapReady }, ref) 
       try {
         const result = await maptilersdk.geocoding.forward(address, {
           language: maptilersdk.Language.RUSSIAN,
-          country: "ru",
+          countries: ["ru"],
           limit: 1,
         });
         const feature = result.features[0];
